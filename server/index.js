0d161be8d6feb596c71c44a0d9112deecb788c98
Taras Labiak <taras.labiak@indeema.com>
2018-12-21 21:05:24 +0200

    + successful payment intent

--- server/index.js
+++ server/index.js
@@ -2,19 +2,35 @@ const express = require("express");
 const config = require("../config");
 const bodyParser = require('body-parser');
 const _ = require("lodash");
+const stripe = require('../stripe');
 
 const app = express();
 
 app.use(express.static(__dirname + "/../public"));
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
-app.get("/config", function(req, res) {
-  res.json(_.pick(config, "publishable", "port"));
+function getClientConfig() {
+  return _.pick(config, "publishable", "port");
+}
+
+app.post("/payment/create", async function(req, res) {
+  if (req.body && req.body.amount >= 50) {
+    const clientConfig = getClientConfig();
+    const intent = await stripe.paymentIntents.create({
+      currency: "usd",
+      allowed_source_types: ["card"],
+      amount: req.body.amount
+    });
+    res.json(Object.assign(clientConfig, {
+      ok: true,
+      client_secret: intent.client_secret
+    }));
+    console.log(intent);
+  }
+  else {
+    res.json({ok: false});
+  }
 });
-// app.post("/receive", function(req, res) {
-//     console.log(req.body);
-//     res.json({ok: true});
-// });
 
 app.listen(config.port || 8085);
